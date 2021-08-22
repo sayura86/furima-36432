a48cdbaad5ccdf4ef08fd0678ff2de1a20bc6235
sayura86 <betterlatethannever52324@gmail.com>
2021-08-22 22:43:05 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Last_name Full-width characters"}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width characters"}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width characters"}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Last_name Full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'First name Full-width characters' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name kana Full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name kana Full-width characters' }
   validates :birthday, presence: true
-  validates :encrypted_password,:password,:password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
 end
